3a1e86de2c89a073dcfaca4c48b53a61decbd9b3
Czarnewski <paulo.czarnewski@scilifelab.se>
2022-09-19 15:10:33 +0200

    fixes code for retrieving pubmed records

--- code/get_pubmed_info.R
+++ code/get_pubmed_info.R
@@ -1,41 +1,4 @@
-#!/home/ubuntu/opt/R/bin/Rscript
 
-library(RISmed)
-library(future)
-library(future.apply)
-```
-
-# Query data from Pubmed
-
-```{r}
-search_topic <- EUtilsQuery("Nilsson Mats")
-search_query <- EUtilsSummary(url = search_topic,type="efetch",db="pubmed")
-summary(search_query)
-search_query@querytranslation <- '("Nilsson Mats"[Full Author Name])'
-
-records <- EUtilsGet(search_query)
-EUt
-records@Query
-#
-# ?EUtilsQuery
-# search_query@querytranslation
-#
-# RISmed::Title(records)
-# RISmed::Title(QueryId(search_query))
-# str(records)
-#
-# for(i in slotNames(records)){print(slot(object = records,name = i))}
-
-
-
-
-# Get journal Impact Factor
-IF <- as.data.frame(readxl::read_xlsx("../data/latestJCRlist2022.xlsx"))
-print(head(IF))
-#colnames(IF) <- IF[1,]
-#IF <- IF[-1,]
-IF$journal_name <- casefold(IF$journal_name)
-print(head(IF))
 
 
 
@@ -43,7 +6,9 @@ print(head(IF))
 reformat_journal_name <- function(journals){
   journals <- casefold(journals)
   journals <- gsub(" [(:.;].*","",journals)
+  journals <- gsub("[-.].*","",journals)
   journals <- gsub("&amp;","&",journals)
+  journals <- gsub("&","and",journals)
   journals <- gsub(",","",journals)
   journals <- gsub("^the ","",journals)
   journals <- gsub("'","",journals)
@@ -56,237 +21,101 @@ fix_names <-  function(x){
   return(x)
 }
 
+#Get Pubmed Journal abreviations isoAbbr
+if(!file.exists("../data/J_Medline.txt")){
+  dir.create("../data",recursive = T)
+  download.file("https://ftp.ncbi.nih.gov/pubmed/J_Medline.txt","../data/J_Medline.txt")
+}
+JA <- readLines("../data/J_Medline.txt")
+JA <- data.frame(
+  JournalTitle = reformat_journal_name(gsub("JournalTitle: ","",JA[grep("JournalTitle",JA)])),
+  IsoAbbr = reformat_journal_name(gsub("IsoAbbr: ","",JA[grep("IsoAbbr",JA)])) )
 
 
-# Match IF for each publication
-terms <- c('Joakim Lundeberg',
-           'Emma Lundberg',
-           'Mats Nilsson',
-           'Sten Linnarsson',
-           'Christos Samakovlis',
-           'Erik Sundström')
-# terms <- c('Czarnewski')
+# Get journal Impact Factor
+IF <- as.data.frame(readxl::read_xlsx("../data/2020ImpactFactordetail.xlsx",skip = 2))
+IF$journal_name <- reformat_journal_name(IF$`Full Journal Title`)
 
-plan(sequential, workers=future::availableCores() )
-all_info <- lapply(terms , function(x){
-  message(paste0('processing:\t',x))
-  x <- fix_names(x)
-  # search_topic <- EUtilsQuery(x)
-  search_query <- EUtilsSummary(x)
-  records <- EUtilsGet(search_query)
-  return(records)
+# Read in output from 'abstract'
+# esearch -db pubmed -query 'Joakim Lundeberg' | efetch -format abstract > ../data/publications/JL.txt
+file_list <- list.files("../data/publications",full.names = T,pattern = ".txt")
+tmp <- lapply( file_list , function(i){
+  a <- readLines(i)
+
+  # Merge all lines into a single string (empty replaced by "BREAKKBREAKK")
+  a[a==""] <- "BREAKK"
+  a <- paste(a , collapse = " ")
+
+  # Split articles
+  b <- strsplit( a , split = "BREAKK BREAKK")[[1]]
+  b <- lapply( b , function(x) {
+    # Remove empty space from start of string
+    x <- gsub("^ ","",x)
+    # Remove 'BREAKK' from start of string
+    x <- gsub("^BREAKK","",x)
+    # Remove query numbers from the start
+    x <- gsub("^[0-9]+?[.] ","",x)
+    return(x)
+  })
+
+  # Split the article information into strings
+  c <- lapply( b , function(x) {
+    x <- strsplit( x , split = "BREAKK")[[1]]
+    # Remove empty space from start of string
+    x <- gsub("^ ","",x)
+    x <- gsub(" $","",x)
+    # Remove query numbers from the start
+    x <- gsub("^[0-9]+?[.] ","",x)
+    return(x)
+  })
+
+
+  # Remove defective entries (some fail to be fetched and are incomplete)
+  c <- c[(1:length(c))[unlist(lapply(c,length)) >= 3]]
+
+  # Select on the JOUNAL, TITLE, AUTHOR_NAMES and PMID
+  c <- lapply( c , function(x) {
+    x <- x[ c(1:3,grep("PMID",x)) ]
+    x <- gsub("[(][0-9]+?[)]","",x)
+    return(x)
+  })
+
+  # Remove defective entries (some fail to be fetched and are incomplete)
+  c <- c[(1:length(c))[unlist(lapply(c,length)) == 4]]
+
+  # Reformat
+  c <- lapply( c , function(x) {
+    z <- c(
+      DATE = sub(".*[.] ","",sub(";.*","",x[1])),
+      PMID = ifelse(grepl("PMID",x[4]),sub(" .*","",sub(".*PMID: ","",x[4])),""),
+      TITLE = x[2],
+      JOURNAL = sub("[.] .*","",x[1]),
+      YEAR = sub(" .*","",sub(".*[.] ","",sub(";.*","",x[1]))),
+      DOI = ifelse(grepl("doi",x[1]),sub("[.] .*","",sub(".*doi: ","",x[1])),"") ,
+      AUTHORS = x[3]
+    )
+    return(z)
+  })
+
+  d <- do.call(rbind,c)
+  d <- as.data.frame(d)
+  d$JOURNAL <- gsub(' [(].*','',d$JOURNAL)
+  d$FULL_JOURNAL_NAME <- JA[ match(reformat_journal_name(d$JOURNAL) , JA$IsoAbbr) , "JournalTitle"]
+
+  d$IF <- IF[ match(reformat_journal_name(d$FULL_JOURNAL_NAME) , IF$journal_name) , "Journal Impact Factor"]
+  return(d)
 })
-names(all_info) <- terms
 
+# Merge all publications
+tmp2 <- do.call(rbind,tmp)
 
-res <- lapply(all_info , function(x){
-  journals <- reformat_journal_name(x@Title)
-  print(journals)
-  xxx <- as.numeric(IF$if_2022[ match( journals , IF$journal_name ) ])
-  return( setNames( xxx , journals )  )
-})
-names(res) <- terms
+# Filter publications
+tmp2 <- tmp2[!is.na(as.numeric(tmp2$IF)),]
+tmp2 <- tmp2[as.numeric(tmp2$IF) > 20,]
+tmp2 <- tmp2[!is.na(tmp2$TITLE),]
+tmp2 <- tmp2[!grepl("Author Correction: ",tmp2$TITLE),]
+tmp2 <- tmp2[!is.na(as.numeric(tmp2$YEAR)),]
+tmp2 <- tmp2[as.numeric(tmp2$YEAR) > 2015,]
+tmp2 <- tmp2[order(as.numeric(tmp2$YEAR),decreasing = T),]
 
-
-
-
-min_if <- 5
-year_cutoff <- 2010
-topN <- 20
-all_pubs <- list()
-
-
-for(i in names(all_info)){
-  message(paste0("Proscessing: ",i))
-
-  sel2 <- all_info[[i]]@YearPubmed >= year_cutoff
-
-  if_cutoff <- sort(res[[i]][sel2],decreasing = T)[ topN ]
-
-  sel <- res[[i]] >= if_cutoff
-  sel3 <- res[[i]] >= min_if
-  sel4 <- !grepl("Publisher[ ]Correction|Author[ ]Correction",all_info[[i]]@ArticleTitle)
-
-  tmp <- all_info[[i]]
-  xxx <- lapply( c("YearPubmed","MonthPubmed","DayPubmed"),function(x) attr(tmp,x) )
-  xxx <- sapply( 1:length(xxx[[1]]) , function(x){
-    paste0(xxx[[1]][x],"-",sprintf("%02d",xxx[[2]][x]),"-",sprintf("%02d",xxx[[3]][x])  ) } )
-  filters <- cbind(sel2,sel,sel3,sel4,
-                   DATE=xxx,
-                   PMID=tmp@PMID,
-                   YEAR=tmp@YearPubmed,
-                   IF=res[[i]],
-                   JOURNAL=gsub(" [(].*","",tmp@Title),
-                   ARTICLE=tmp@ArticleTitle,
-                   AUTHORS=  sapply(tmp@Author,function(x){
-                     x[is.na(x)] <- ""
-                     z <- paste0(x$CollectiveName,paste0(x$LastName," ",x$Initials) )
-                     z  <- paste0(z,collapse = ", ")
-                     return(z)
-                   }) )
-
-  sels <- grep( "1", as.logical(filters[,1])*as.logical(filters[,2])*as.logical(filters[,3])*as.logical(filters[,4]) )
-  all_pubs[[i]] <- filters[sels,]
-
-  con <- file(paste0("../compiled/publications/",i,'.txt'))
-  sink(con,append=TRUE)
-  sink(con,append=TRUE,type="message")
-
-  for(j in sels){
-    k <- tmp@PMID[j]
-    tmp2 <- tmp@Author[[k]]
-    tmp2[is.na(tmp2)] <- ""
-    tmp2$LastName <- paste0(tmp2$CollectiveName,tmp2$LastName)
-
-    x <- paste0(paste0( tmp2$LastName ," ", tmp2$Initials ,collapse = ", "),". ")
-    x1 <- paste0(sub(".* ","",i), " ", substring(sub(" .*","",i), 1, 1) )
-    if( grepl( paste0(x1,"[,.] "), x ) ){
-      x <- gsub(x1 , paste0("<u>",x1,"</u>"), x)
-    }
-
-    cat( "<p>" )
-    cat( paste0( x ) )
-    cat( paste0("<b>", tmp@ArticleTitle[[j]],"</b>") )
-    cat( "\n" )
-    cat( paste0("<a href='https://doi.org/",tmp@DOI[[j]],"'>") )
-    cat( paste0( tmp@Title[[j]]) )
-    cat( "</a>" )
-    cat( paste0( " (" , month.abb[ tmp@MonthPubmed[[j]] ]," ", tmp@YearPubmed[[j]],") " ) )
-
-    cat( "</p>" )
-    cat( "\n\n" )
-
-  }
-
-  sink()
-  sink(type="message")
-}
-```
-
-
-```{r}
-all_pubs
-
-
-```
-
-
-
-
-
-```{r}
-# install.packages('scholar')
-# library(scholar)
-#
-# terms <- c(JoakimLundeberg='kDUHVMIAAAAJ',
-#            EmmaLundberg='yHnDnvcAAAAJ',
-#            MatsNilsson='M6g0gRsAAAAJ',
-#            StenLinnarsson='Bk1l6V0AAAAJ',
-#            ChristosSamakovlis='xYqJw8wAAAAJ',
-#            ErikSundström='')
-#
-#
-#
-# res <- scholar::get_publications(id = 'kDUHVMIAAAAJ')
-
-#
-# baseurl <- 'https://api.biorxiv.org/details/biorxiv/'
-# download.file(
-#   url = paste0(baseurl,Sys.Date() - 30*6,'/',Sys.Date(),''),
-#   destfile = '../data/tmp.txt')
-#
-# res <- jsonlite::fromJSON('../data/tmp.txt')$collection
-# dim(res)
-
-
-
-
-```{r}
-# library(medrxivr)
-#
-#
-# plan(multiprocess, workers=future::availableCores()-1 )
-# options(future.globals.maxSize = 1500 * 1024 ^ 2)
-# preprint_data_list <- future_lapply(1:10,function(i){
-#   return(mx_api_content(
-#   server = "biorxiv",
-#   from_date = as.character(Sys.Date() - 10*i),
-#   to_date = as.character(Sys.Date() - 10*(i-1) )))
-# })
-#
-# options(timeout = 10003)
-#
-# terms_use <- c("Lundeberg, J[.]","Lundberg, E[.]","Nilsson, M[.]","Linnarsson, S[.]","Samakovlis, C[.]","Sundström, E[.]"  )
-# res <- preprint_data [ grepl( paste0(terms_use,collapse = "|") , preprint_data$authors) ,]
-# res$authors
-# res$date
-```
-
-
-```{r}
-all_pubs2 <- as.data.frame( do.call( rbind , all_pubs ) )
-all_pubs2 <- all_pubs2[ ,-c(1:4) ]
-all_pubs2 <- all_pubs2[ ! duplicated(all_pubs2) , ]
-all_pubs2 <- all_pubs2[ as.numeric( all_pubs2$IF ) > 10 , ]
-all_pubs2 <- all_pubs2[ as.numeric( all_pubs2$YEAR ) > 2019 , ]
-all_pubs2 <- all_pubs2[ order( all_pubs2$YEAR , decreasing = T) , ]
-
-dim(all_pubs2)
-
-all_pubs2$JOURNAL
-
-tmp <- all_info[[1]]
-for(i in slotNames(tmp)){
-  valll <- lapply(all_info ,i=i, function(x,i) return(slot(x,i)))
-
-  print(typeof(slot(tmp,i) ))
-  print(typeof(valll[[1]]))
-
-  if( is.list( valll[[1]] ) ){
-    valll <- do.call(c,valll)
-    names(valll) <- sub(".*[.]","",names(valll))
-    slot(tmp, i) <- valll
-  } else {
-    slot(tmp, i) <- unlist(valll)
-  }
-}
-write.csv(all_pubs2 , "../compiled/publications/paper_news2.csv",row.names = F)
-
-
-
-
-con <- file(paste0('../compiled/publications/paper_news.txt'))
-sink(con,append=TRUE)
-sink(con,append=TRUE,type="message")
-sink(con,append=TRUE,type="message")
-cat( "<h3>RECENT PUBLICATIONS</h3>\n\n" )
-for(j in match( all_pubs2$PMID , tmp@PMID ) ){
-  k <- tmp@PMID[j]
-  cat( "<p>" )
-  cat( paste0("<b>",tmp@YearPubmed[[j]],"-",tmp@MonthPubmed[[j]],"-",tmp@DayPubmed[[j]] ,"</b>\n") )
-  cat( paste0("<b>", tmp@Title[[j]] , "</b>. ") )
-  cat( paste0("<a href='https://doi.org/",tmp@DOI[[j]],"'>") )
-  cat( paste0( tmp@ArticleTitle[[j]] ) )
-  cat( "</a>" )
-  # cat( "\n" )
-
-  tmp2 <- tmp@Author[[k]]
-  tmp2[is.na(tmp2)] <- ""
-  tmp2$LastName <- paste0(tmp2$CollectiveName,tmp2$LastName)
-
-  x <- paste0(paste0( tmp2$LastName ," ", tmp2$Initials ,collapse = ", "),".")
-  x1 <- paste0(sub(".* ","",i), " ", substring(sub(" .*","",i), 1, 1) )
-  if( grepl( paste0(x1,"[,.] "), x ) ){
-    x <- gsub(x1 , paste0("<u>",x1,"</u>"), x)
-  }
-  cat( paste0(". ", x ) )
-  cat( "</p>" )
-
-  cat( "\n\n" )
-
-}
-  cat( "<hr>" )
-  cat( "\n\n" )
-
-sink()
-sink(type="message")
+write.csv(tmp2,"../compiled/publications/paper_news.csv")
---
+++ code/update_publications.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+mkdir -p ../data/publications
+mkdir -p ../compiled/publications
+
+echo 'Joakim Lundeberg'
+# esearch -db pubmed -query 'Joakim Lundeberg' | efetch -format native -mode xml > ../data/publications/JL.xml
+esearch -db pubmed -query 'Joakim Lundeberg' | efetch -format abstract > ../data/publications/JL.txt
+
+echo 'Emma Lundberg'
+# esearch -db pubmed -query 'Emma Lundberg' | efetch -format native -mode xml > ../data/publications/EM.xml
+esearch -db pubmed -query 'Emma Lundberg' | efetch -format abstract > ../data/publications/EM.txt
+
+echo 'Mats Nilsson'
+# esearch -db pubmed -query 'Mats Nilsson' | efetch -format native -mode xml > ../data/publications/MN.xml
+esearch -db pubmed -query 'Mats Nilsson' | efetch -format abstract > ../data/publications/MN.txt
+
+echo 'Sten Linnarsson'
+# esearch -db pubmed -query 'Sten Linnarsson' | efetch -format native -mode xml > ../data/publications/SL.xml
+esearch -db pubmed -query 'Sten Linnarsson' | efetch -format abstract > ../data/publications/SL.txt
+
+echo 'Christos Samakovlis'
+# esearch -db pubmed -query 'Christos Samakovlis' | efetch -format native -mode xml > ../data/publications/CS.xml
+esearch -db pubmed -query 'Christos Samakovlis' | efetch -format abstract > ../data/publications/CS.txt
+
+echo 'Erik Sundstrom'
+# esearch -db pubmed -query 'Erik Sundstrom' | efetch -format native -mode xml > ../data/publications/ES.xml
+esearch -db pubmed -query 'Erik Sundstrom' | efetch -format abstract > ../data/publications/ES.txt
+
+Rscript ./get_pubmed_info.R
